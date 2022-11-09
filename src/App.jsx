91fd1385d926e2242839b78e16f099cc36a7e0f1
mariosepulveda3 <mariosepul3@gmail.com>
2022-11-09 17:33:07 +0100

    ADD, useState Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,30 @@
 import "./App.scss";
 import { CV } from "./utils/cv";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import Hero from "./components/Hero";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import Habilities from "./components/Habilities";
-import Languages from "./components/Languages";
-import mario from "./assets/img/imgny.jpg";
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./pages/Hero";
+import Menu from "./components/Menu";
 import Footer from "./components/Footer";
-// import { info } from "sass";
+import Aside from "./components/Aside";
 
 const App = () => {
-  const { hero, education, languages, habilities, experience } = CV;
+  
+  const { hero } = CV;
 
   return (
     <div className="app">
       <nav>
-        <a href="https://github.com/mariosepulveda3" target="blank">
-          My GitHub
-        </a>
-        <a
-          href="https://www.linkedin.com/in/mario-sep%C3%BAlveda-donoso-1aa959142"
-          target="blank"
-        >
-          LinkedIn
-        </a>
-        <a href="d">Projects</a>
+        <Navbar />
       </nav>
       <aside>
-        <img src={mario} alt="profile" />
-        <Contact info={hero} />
+      <Aside/>
       </aside>
       <main>
         <Hero info={hero} />
-        <About metadata={hero.aboutMe} />
-        <Education education={education} />
-        <Experience experience={experience} />
-        <Languages languages={languages} />
-        <Habilities habilities={habilities} />
+        <Menu />
       </main>
       <footer>
-        <Footer info={hero} />
+        <Footer />
       </footer>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,9 @@
-import "./Footer.scss";
+import "./styles/Footer.scss";
 
 const Footer = ({ info }) => {
   return (
     <div className="footer">
       <h3>CV CREATED WITH REACT</h3>
-      <div className="info">
-        <h4>{info.city} -</h4>
-        <h4>- {info.birthDate}</h4>
-      </div>
     </div>
   );
 };
