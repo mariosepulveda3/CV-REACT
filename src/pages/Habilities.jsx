72c8668af72f906e3d77ec8a4b6271f4f73e6d69
mariosepulveda3 <mariosepul3@gmail.com>
2023-03-08 09:39:21 +0100

    FIX: Change in funcionality changeHability in Habilities Component, before was randomChange, and now is with count +1

--- src/pages/Habilities.jsx
+++ src/pages/Habilities.jsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 import "./styles/Habilities.scss";
 
 const Habilities = ({ habilities }) => {
+  const numHabilities = habilities.length;
   const [hability, setHability] = useState(habilities[0]);
+  const [habilityIndex, setHabilityIndex] = useState(0);
 
   const changeHab = () => {
-    const positionHab = Math.floor(Math.random() * habilities.length);
-    setHability(habilities[positionHab]);
+    const positionHability = habilityIndex + 1;
+    if (positionHability >= numHabilities) {
+      setHabilityIndex(0);
+      setHability(habilities[0]);
+    } else {
+      setHabilityIndex(positionHability);
+      setHability(habilities[positionHability]);
+    }
   };
 
   return (
     <div className="habilities">
       <h2>Skills</h2>
-      <button onClick={changeHab}>changeSkill</button>
+      <button onClick={changeHab}>Change Skill</button>
       <p>{hability}</p>
     </div>
   );
